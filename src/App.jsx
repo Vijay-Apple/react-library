c3ffcab37ffd38c085ab252fd8ad4c0a5853c5c7
Vijay Rajput <your_github_email@gmail.com>
2026-01-29 10:20:54 +0530

    setGoogleNumberCounter

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [showCount, setShowCount] = useState(15);
+  let increaseByOne = () => {
+    showCount = showCount + 1;
+    setShowCount(showCount);
+    console.log(showCount);
+    if (showCount === 100) {
+      let afterEnter20 = "you are able to increase only 20 ";
+      setShowCount(afterEnter20);
+    }
+  };
+  let decreaseByOne = () => {
+    showCount = showCount - 1;
+    setShowCount(showCount);
+    console.log(showCount);
+    if (showCount === 0) {
+      let negative_value_entering = "you are not able to Enter negative number";
+      setShowCount(negative_value_entering);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Google Count</h1>
+      <br />
+      <h2>show google count: {showCount}</h2>
+
+      <button onClick={increaseByOne}>increaseByOne</button>
+      <br />
+      <br />
+      <button onClick={decreaseByOne}>decreaseByOne</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
